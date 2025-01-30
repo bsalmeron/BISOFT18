4abd58fa987e2bcafb33a68dff605548799391d4
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2025-01-29 18:45:34 -0600

    Update Soluccion Practica semana 1.sql

--- Semana 2/Soluccion Practica semana 1.sql
+++ Semana 2/Soluccion Practica semana 1.sql
@@ -1,1 +1,21 @@
---Hola
+ --1
+ Use Northwind
+
+ Select P.ProductName, C.CategoryName  from Products P 
+ INNER JOIN Categories  C
+ ON P.CategoryID = c.CategoryID
+
+ Select O.OrderID, o.OrderDate,P.ProductName from Customers C
+ INNER JOIN Orders O 
+ ON C.CustomerID = O.CustomerID 
+ INNER JOIN [Order Details] OD
+ ON o.OrderID = OD.OrderID
+ INNER JOIN Products P 
+ ON OD.ProductID = P.ProductID
+ where C.CustomerID='ALFKI'
+
+ -- 6 
+
+ Select * from Products
+ Where ProductName Like '%Chai%'
+
