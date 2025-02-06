ad9af64f7d0dd14c59e80719ef7e6257384435a2
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2025-02-05 18:49:24 -0600

    Create Conceptos TDE y Enmascaramiento.sql

---
+++ Semana 3/Conceptos TDE y Enmascaramiento.sql
@@ -0,0 +1,8 @@
+Brayner
+
+TDE - Cifrado : $"·"!%%&&&&"12342
+AES256 -- Certificado +  Una contraseña (mancomunada)
+
+DMM - Enmascaramiento :BXXXXr ---> Tabla 
+
+Nombre varchar(20) Masked with (funtion = 'parcial(1,xxxxxx,3)')
