6b6d84c62d3025ead79c92d45f7bf5ae154036f8
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2025-02-12 19:32:48 -0600

    Update Joins Group by Having.sql

--- Semana 4/Joins Group by Having.sql
+++ Semana 4/Joins Group by Having.sql
@@ -8,6 +8,16 @@ order by C.CategoryName desc
 
 --2 Realizar una consulta que muestre el nombre de contacto, la orden, 
 --y datos entrega 
-
+SELECT C.ContactName,O.OrderID,O.ShipAddress,O.Freight,CONVERT(DATE,O.ShippedDate) 
+DATE from Customers C
+INNER JOIN ORDERS O 
+ON C.CustomerID = O.CustomerID
 -- 3 Realizar una consulta que muestre los clientes (id, nombre compañia)
 -- el numero de orden, y muestre el nombre del empleado que la atendio
+SELECT C.CUSTOMERID,C.CompanyName,O.OrderID,CONCAT(E.FirstName,' ',E.LastName)
+NAME FROM CUSTOMERS C
+INNER JOIN ORDERS O
+ON C.CustomerID = O.CustomerID
+INNER JOIN EMPLOYEES E 
+ON O.EmployeeID = E.EmployeeID
+ORDER BY C.CustomerID
