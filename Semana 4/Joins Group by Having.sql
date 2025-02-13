b4ab127e53f1685ac081215a7cf25232430aad81
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2025-02-12 19:09:00 -0600

    Update Joins Group by Having.sql

--- Semana 4/Joins Group by Having.sql
+++ Semana 4/Joins Group by Having.sql
@@ -1,9 +1,13 @@
 USE Northwind
 
---Mostrar la categoria del producto 
+--1 Mostrar la categoria del producto 
 SELECT  P.ProductName, C.CategoryName FROM Products P 
 INNER JOIN Categories C
 ON P.CategoryID = C.CategoryID
 order by C.CategoryName desc
 
---
+--2 Realizar una consulta que muestre el nombre de contacto, la orden, 
+--y datos entrega 
+
+-- 3 Realizar una consulta que muestre los clientes (id, nombre compañia)
+-- el numero de orden, y muestre el nombre del empleado que la atendio
